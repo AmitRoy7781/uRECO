b530a24cc49aaf6689fe50c52caa7df1f3927c7f
Saif Mahmud Dhrubo <you@example.com>
2018-03-29 13:04:33 +0600

    Facial Expression Classifier Final

---
+++ FacialExpressionClassifier/genScaledDat.py
@@ -0,0 +1,14 @@
+#script for preparing datasets, loading fer2013 data and generating scaled images
+import pandas as pd
+import numpy as np
+
+#loading fer2013.csv
+
+data = pd.read_csv('fer2013.csv')
+data = data['pixels']
+data = [dat.split() for dat in data]
+data = np.array(data)
+data = data.astype('float64')
+data = [[np.divide(d, 255.0) for d in dat] for dat in data]
+
+np.save('/home/saif_m_dhrubo/Study Materials/CSE DU - 3_1/CSE-3112 Software Engineering Lab/Project/webCamEmocognizer/data/Scaled.bin.npy',data)

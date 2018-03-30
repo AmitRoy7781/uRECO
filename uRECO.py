72d5159a2e69a06db3915ae13ccda0b453e33b92
AmitRoy7 <aroy7298@gmail.com>
2018-03-30 17:24:15 +0600

    Added new feature Holographic Keyboard

--- uRECO.py
+++ uRECO.py
@@ -1,18 +1,57 @@
-from flask import Flask,render_template,flash,redirect,url_for,session,logging
+from flask import Flask, render_template, request, json, flash, redirect, url_for, session, logging
 from flask_mysqldb import MySQL
-from wtforms import Form,StringField,TextAreaField,PasswordField,validators
-#from passlib.hash import sha256_cryptf
 
+mysql = MySQL()
 app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'TishuPaper'
+app.config['MYSQL_DATABASE_DB'] = 'uRECO'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 
 @app.route('/')
-def main():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
 
-@app.route('/sign_up')
+
+@app.route('/show_sign_up')
+def showSignUp():
+    return render_template('sign_up.html')
+
+
+@app.route('/sign_up', methods=['POST', 'GET'])
 def sign_up():
-    return render_template("sign_up.html")
+    try:
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['pass']
+
+        if name and email and password:
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            # _hashed_password = generate_password_hash(password)
+            cursor.callproc('sp_createUser', (name, email, password))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message': 'User created successfully !'})
+            else:
+                return json.dumps({'error': str(data[0])})
+        else:
+            return json.dumps({'html': '<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error': str(e)})
+
+    finally:
+        cursor.close()
+        conn.close()
+    render_template("sign_up.html")
 
 @app.route('/sign_in')
 def sign_in():

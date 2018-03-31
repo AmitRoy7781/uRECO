685d281848e76dbe29c094fe6103311676553a66
Saif-M-Dhrubo <saif.dhrubo@gmail.com>
2018-03-31 10:02:49 +0600

    Facial Expression Classifier v3.0

---
+++ FacialExpressionClassifier/genScaledDat.py
@@ -0,0 +1,14 @@
+#script for preparing datasets, loading fer2013 data and generating scaled images
+import pandas as pd
+import numpy as np
+
+#loading fer2013.csv
+
+data = pd.read_csv('fer2013.csv') #kaggle datasets #https://www.kaggle.com/c/challenges-in-representation-learning-facial-expression-recognition-challenge
+data = data['pixels']
+data = [dat.split() for dat in data]
+data = np.array(data)
+data = data.astype('float64')
+data = [[np.divide(d, 255.0) for d in dat] for dat in data]
+
+np.save('/home/saif_m_dhrubo/uRECO/FacialExpressionClassifier/data/Scaled.bin.npy',data)

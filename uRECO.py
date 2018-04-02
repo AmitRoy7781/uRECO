de66c4341d4d6abc774c5b3478f92387ea0a0d7c
Tauhid13 <tauhidtanjim13@gmail.com>
2018-04-03 00:58:53 +0600

    signup included

--- uRECO.py
+++ uRECO.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, json, flash, redirect, url_for, session, logging
 from flask_mysqldb import MySQL
+import MySQLdb
+
 
 mysql = MySQL()
 app = Flask(__name__)
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'TishuPaper'
-app.config['MYSQL_DATABASE_DB'] = 'uRECO'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+conn = MySQLdb.connect(host="localhost",user="root",password="tauhid 123",db="uRECO")
+
 mysql.init_app(app)
 
 
@@ -22,36 +22,26 @@ def showSignUp():
     return render_template('sign_up.html')
 
 
-@app.route('/sign_up', methods=['POST', 'GET'])
+@app.route('/sign_up', methods=['POST'])
 def sign_up():
-    try:
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['pass']
 
-        if name and email and password:
 
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            # _hashed_password = generate_password_hash(password)
-            cursor.callproc('sp_createUser', (name, email, password))
-            data = cursor.fetchall()
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["pass"]
 
-            if len(data) is 0:
-                conn.commit()
-                return json.dumps({'message': 'User created successfully !'})
-            else:
-                return json.dumps({'error': str(data[0])})
-        else:
-            return json.dumps({'html': '<span>Enter the required fields</span>'})
 
-    except Exception as e:
-        return json.dumps({'error': str(e)})
 
-    finally:
-        cursor.close()
-        conn.close()
-    render_template("sign_up.html")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO user (user_username,user_password,user_email)VALUES(%s,%s,%s)", (name, password, email))
+        conn.commit()
+        return render_template("home.html")
+
+
+@app.route('/show_sign_in')
+def showSignIn():
+    return render_template('sign_in.html')
+
 
 @app.route('/sign_in')
 def sign_in():
